c8dffa658080743ac7d8d09b02f2fcae5484d604
GoatLeywin <121911187+GoatLeywin@users.noreply.github.com>
2024-12-17 10:51:01 -0600

    Update ModItems.java

--- src/main/java/net/goatmorreti/create_new_tempest/item/ModItems.java
+++ src/main/java/net/goatmorreti/create_new_tempest/item/ModItems.java
@@ -11,17 +11,17 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, CreateNewTempest.MOD_ID);
 
-    public static  final RegistryObject<Item> PINK_SLIME_POOP = ITEMS.register("pink_slime_poop",
+    public static final RegistryObject<Item> PINK_SLIME_POOP = ITEMS.register("pink_slime_poop",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CREATE_NEW_TEMPEST_TAB)));
-    public static  final RegistryObject<Item> RED_SLIME_POOP = ITEMS.register("red_slime_poop",
+    public static final RegistryObject<Item> RED_SLIME_POOP = ITEMS.register("red_slime_poop",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CREATE_NEW_TEMPEST_TAB)));
-    public static  final RegistryObject<Item> TEAL_SLIME_POOP = ITEMS.register("teal_slime_poop",
+    public static final RegistryObject<Item> TEAL_SLIME_POOP = ITEMS.register("teal_slime_poop",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CREATE_NEW_TEMPEST_TAB)));
-    public static  final RegistryObject<Item> BLUE_SLIME_POOP = ITEMS.register("blue_slime_poop",
+    public static final RegistryObject<Item> BLUE_SLIME_POOP = ITEMS.register("blue_slime_poop",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CREATE_NEW_TEMPEST_TAB)));
-    public static  final RegistryObject<Item> YELLOW_SLIME_POOP = ITEMS.register("yellow_slime_poop",
+    public static final RegistryObject<Item> YELLOW_SLIME_POOP = ITEMS.register("yellow_slime_poop",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CREATE_NEW_TEMPEST_TAB)));
-    public static  final RegistryObject<Item> GREEN_SLIME_POOP = ITEMS.register("green_slime_poop",
+    public static final RegistryObject<Item> GREEN_SLIME_POOP = ITEMS.register("green_slime_poop",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CREATE_NEW_TEMPEST_TAB)));
 
     public static void register(IEventBus eventBus) {
